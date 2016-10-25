b7fb51b2283a5a878ce93834da7caaf47e8c0d17
Kevin Mahoney <kjmahoney13@gmail.com>
2016-10-25 19:11:52 -0400

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,26 @@
 "use strict";
 
-(function(){
-  angular.module("wdinstagram", [])
-})();
+// (function(){
+//   angular.module("wdinstagram", [])
+// })();
+
+angular.module("wdinstagram", ["ui.router"])
+.config(["$stateProvider", Router])
+.controller("InstaIndexController", [
+  InstaIndexControllerFunction
+]);
+
+function InstaIndexControllerFunction(){
+  console.log("controllerWorking")
+}
+
+function Router($stateProvider){
+  $stateProvider
+  .state("instaIndex", {
+    url: "/instagrams",
+    templateUrl: "js/ng-views/index.html",
+    controller: "InstaIndexController",
+    controllerAs: "vm"
+  });
+
+}
