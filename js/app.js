5d2fa7851fc996d215190993a5cb350ea175b756
Kevin Mahoney <kjmahoney13@gmail.com>
2016-10-26 14:33:51 -0400

    added dummy show

--- js/app.js
+++ js/app.js
@@ -6,21 +6,55 @@
 
 angular.module("wdinstagram", ["ui.router"])
 .config(["$stateProvider", Router])
-.controller("InstaIndexController", [
-  InstaIndexControllerFunction
-]);
+.controller("GramIndexController", [
+  GramIndexControllerFunction
+])
+.controller("GramShowController", [
+  "$stateParams",
+  GramShowControllerFunction
+])
 
-function InstaIndexControllerFunction(){
-  console.log("controllerWorking")
+function GramIndexControllerFunction(){
+  console.log("indexcontrollerWorking")
+  this.grams = grams;
 }
+
+function GramShowControllerFunction($stateParams){
+  console.log("showcontrollerWorking")
+
+  this.gram = grams[$stateParams.id]
+  console.log($stateParams)
+  console.log(this.gram)
+}
+
 
 function Router($stateProvider){
   $stateProvider
-  .state("instaIndex", {
+  .state("gramIndex", {
     url: "/instagrams",
     templateUrl: "js/ng-views/index.html",
-    controller: "InstaIndexController",
+    controller: "GramIndexController",
+    controllerAs: "vm"
+  })
+  .state("gramShow",{
+    url:"/instagrams/:id",
+    templateUrl: "js/ng-views/show.html",
+    controller: "GramShowController",
     controllerAs: "vm"
   });
 
 }
+
+var grams = [
+  {
+    photo_url: "#",
+    author: "Ludacris",
+    body: "Roll out!"
+
+  },
+  {
+    photo_url: "#",
+    author: "DMX",
+    body: "Roll out!"
+  }
+]
